0a7ac3bae19e7fe1c1b96fa3bdfc4f374786ec72
Shakti Prakash Das <Dasshakti659@gmail.com>
2026-02-20 23:32:57 +0530

    feat(Added FoodItemRepositoty JPA Interface)

--- src/main/java/com/shakti/Controller.java
+++ src/main/java/com/shakti/Controller.java
@@ -1,12 +1,27 @@
 package com.shakti;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    @Autowired
+    FoodItemRepository repository;
+
     @GetMapping("/hello")
     String hello(){
         return "Hello World";
+    }
+
+    @GetMapping("/add")
+    void add(){
+        FoodItem foodItem=new FoodItem();
+        foodItem.itemName="Food Item-1";
+        foodItem.quantity=1;
+        foodItem.price=190;
+
+        repository.save(foodItem);
     }
 }
---
+++ src/main/java/com/shakti/FoodItemRepository.java
@@ -0,0 +1,9 @@
+package com.shakti;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodItemRepository extends JpaRepository <FoodItem,String> {
+
+}
